4c01583ae829b33377d68f6c742e1580a2f27a48
EzhilarasiBaladhandapani <51845006+EzhilarasiBaladhandapani@users.noreply.github.com>
2019-06-27 16:30:16 +0530

    Add files via upload

--- even.py
+++ even.py
@@ -1,9 +1,5 @@
 a=int(input())
 b=int(input())
-for i in range(a,b):
-
-	if i%2!= 0:
-		print('odd numbers are ',i)
-for j in range(a,b):
-	if j%2==0:
-		print('even numbers are ',j)
+for i in range(a+1,b):
+	if(i%2==0):
+		print(i)
